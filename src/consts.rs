5a9500da29433d944b2ac65ec3511b8198f9fbf7
ndornseif <5382403+ndornseif@users.noreply.github.com>
2025-03-13 23:41:06 +0100

    Replaced use of unsafe transmute with ::from_bits()

--- src/consts.rs
+++ src/consts.rs
@@ -39,67 +39,63 @@
 
 /// Double precision (f64) constants.
 pub mod double {
-    use core::mem::transmute;
-
     /// One plus the square root of two, also known as the silver ratio.
     /// The positive solution of the equation x^2 = 2x + 1.
     /// Exact double representation: 2.41421356237309492343001693370752036571502685546875
-    pub const SQRT_2_PLUS_1: f64 = unsafe { transmute::<u64, f64>(0x4003504f333f9de6) };
+    pub const SQRT_2_PLUS_1: f64 = f64::from_bits(0x4003504f333f9de6);
 
     /// The cube root of two  
     /// Exact double representation: 1.2599210498948731906665443602832965552806854248046875
-    pub const CBRT_2: f64 = unsafe { transmute::<u64, f64>(0x3ff428a2f98d728b) };
+    pub const CBRT_2: f64 = f64::from_bits(0x3ff428a2f98d728b);
 
     /// The cube root of three  
     /// Exact double representation: 1.442249570307408301772511549643240869045257568359375
-    pub const CBRT_3: f64 = unsafe { transmute::<u64, f64>(0x3ff7137449123ef6) };
+    pub const CBRT_3: f64 = f64::from_bits(0x3ff7137449123ef6);
 
     /// The supergolden ratio. The real solution of the equation x^3 = x^2 + 1.  
     /// Exact double representation: 1.4655712318767679658293445754679851233959197998046875
-    pub const SUPERGOLDEN_RATIO: f64 = unsafe { transmute::<u64, f64>(0x3ff772fad1ede80b) };
+    pub const SUPERGOLDEN_RATIO: f64 = f64::from_bits(0x3ff772fad1ede80b);
 
     /// The supersilver ratio. The real solution of the equation x^3 = 2x^2 + 1.  
     /// Exact double representation: 2.205569430400590391627702047117054462432861328125
-    pub const SUPERSILVER_RATIO: f64 = unsafe { transmute::<u64, f64>(0x4001a50195e505e8) };
+    pub const SUPERSILVER_RATIO: f64 = f64::from_bits(0x4001a50195e505e8);
 
     /// One over 2 to the 53th power. Equivalent to 1.0 / (1u64 << 53) as f64.
     /// Exact double representation: 1.1102230246251565404236316680908203125E-16
-    pub const INV_2POW53: f64 = unsafe { transmute::<u64, f64>(0x3ca0000000000000) };
+    pub const INV_2POW53: f64 = f64::from_bits(0x3ca0000000000000);
 
     /// One over 2 to the 24th power. Equivalent to 1.0 / (1u64 << 24) as f64.
     /// Exact double representation: 5.9604644775390625E-8
-    pub const INV_2POW24: f64 = unsafe { transmute::<u64, f64>(0x3e70000000000000) };
+    pub const INV_2POW24: f64 = f64::from_bits(0x3e70000000000000);
 }
 
 /// Single precision (f32) constants.
 pub mod float {
-    use core::mem::transmute;
-
     /// One plus the square root of two, also known as the silver ratio.  
     /// Exact float representation: 2.414213657379150390625
-    pub const SQRT_2_PLUS_1: f32 = unsafe { transmute::<u32, f32>(0x401a827a) };
+    pub const SQRT_2_PLUS_1: f32 = f32::from_bits(0x401a827a);
 
     /// The cube root of two  
     /// Exact float representation: 1.25992107391357421875
-    pub const CBRT_2: f32 = unsafe { transmute::<u32, f32>(0x3fa14518) };
+    pub const CBRT_2: f32 = f32::from_bits(0x3fa14518);
 
     /// The cube root of three  
     /// Exact float representation: 1.4422495365142822265625
-    pub const CBRT_3: f32 = unsafe { transmute::<u32, f32>(0x3fb89ba2) };
+    pub const CBRT_3: f32 = f32::from_bits(0x3fb89ba2);
 
     /// The supergolden ratio. The real solution of the equation x^3 = x^2 + 1.  
     /// Exact float representation: 1.46557128429412841796875
-    pub const SUPERGOLDEN_RATIO: f32 = unsafe { transmute::<u32, f32>(0x3fbb97d7) };
+    pub const SUPERGOLDEN_RATIO: f32 = f32::from_bits(0x3fbb97d7);
 
     /// The supersilver ratio. The real solution of the equation x^3 = 2x^2 + 1.  
     /// Exact float representation: 2.2055695056915283203125
-    pub const SUPERSILVER_RATIO: f32 = unsafe { transmute::<u32, f32>(0x400d280d) };
+    pub const SUPERSILVER_RATIO: f32 =f32::from_bits(0x400d280d);
 
     /// One over 2 to the 53th power. Equivalent to 1.0 / (1u64 << 53) as f32.
     /// Exact double representation: 1.1102230246251565404236316680908203125E-16
-    pub const INV_2POW53: f32 = unsafe { transmute::<u32, f32>(0x25000000) };
+    pub const INV_2POW53: f32 = f32::from_bits(0x25000000);
 
     /// One over 2 to the 24th power. Equivalent to 1.0 / (1u64 << 24) as f32.
     /// Exact double representation: 5.9604644775390625E-8
-    pub const INV_2POW24: f32 = unsafe { transmute::<u32, f32>(0x33800000) };
+    pub const INV_2POW24: f32 = f32::from_bits(0x33800000);
 }
